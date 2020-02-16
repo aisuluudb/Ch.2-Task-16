8174032abd2ad67e98c68293674ee1f85b856617
Aisuluu DB <dzhaisuluu@gmail.com>
2020-02-16 21:31:29 +0600

    Add files via upload

---
+++ Task16.py
@@ -0,0 +1,5 @@
+earth_weight = int(input("Input your weight"))
+for year in range (1, 16, 1):
+     earth_weight = earth_weight + 1
+     moonm = earth_weight * 0.165
+     print(f'Year {year} my weight on te moon will be {moonm}')
